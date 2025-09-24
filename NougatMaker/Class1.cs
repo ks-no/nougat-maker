0dac2ff3a3864c7a9711c91a49e4076e971a8eb8
Philippe Haavik <phil98h@hotmail.no>
2025-09-24 10:02:22 +0200

    Mmm nougat

---
+++ NougatMaker/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace NougatMaker;
+
+using System.Reflection;
+
+public class Class1
+{
+    public static Stream? GetResourceStream(string resourceName)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        // Example: "NougatMaker.Resources.NougatChristmassy.jpeg"
+        return assembly.GetManifestResourceStream($"NougatMaker.Resources.{resourceName}");
+    }
+}
